b004ddee92bce64584d50c2ac31be67561ef916c
Philippe Gregoire <philippe.gregoire@fr.ibm.com>
2020-05-15 02:24:00 +0200

    Add AutoAI prediction

--- goodvibrations/predictStatus.py
+++ goodvibrations/predictStatus.py
@@ -19,20 +19,43 @@ logger = logging.getLogger(__name__)
 token= '2c25e35482cff0323dc9c5b0051b7413918b9fdd'
 PACKAGE_URL = f"https://{token}@github.com/philippe-gregoire/iotfgoodvibs@starter_package"
 
+import autoai_libs
+
 class PredictStatus_ICare(BaseTransformer):
 
     def __init__(self, input_items, predStatus, output_items):
         self.input_items = input_items
         self.output_items = output_items
         self.predStatus = str(predStatus)
+
+        import pickle
+        import io
+        import os
+        import goodvibrations
+
+        FILE='VE-1138_All'
+        pn='P3'
+        #target='Status'
+
+        self.pipes={}
+        for p in ('preproc','feateng','predict'):
+            with io.open(f"{os.path.join(os.path.dirname(goodvibrations.__file__),FILE)}_P{pn}_{p}.pickle",'rb') as f:
+                self.pipes[p]=pickle.loads(f.read())
+        print("Loaded models")
+
         super().__init__()
 
     def execute(self, df):
         print("Enter Execute")
-        df = df.copy()
-        print("Apply factor")
-        for i,input_item in enumerate(self.input_items):
-            df[self.output_items[i]] = self.predStatus
+        #df = df.copy()
+        print(f"Apply factor to {len(self.input_items)}")
+
+        preV=self.pipes['preproc'].transform(df.values)
+        feV=self.pipes['feateng'].transform(preV)
+        predV=self.pipes['predict'].predict(feV)
+        print(f"predicted values={predV}")
+        for i,input_item in enumerate(self.output_items):
+            df[self.output_items[i]] = predV
         return df
 
     @classmethod
